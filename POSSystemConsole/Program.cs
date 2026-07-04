0eaa17eddb205378e0b3f35963bae43268fbd380
mtdayo <lawrightmt@gmail.com>
2026-07-04 09:08:35 +0900

    プッシュ

---
+++ POSSystemConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+
+string? input;
+
+do
+{
+    Console.Write("a:");
+    input = Console.ReadLine();     
+
+    if (input == "exit")
+    {
+        Console.WriteLine("Exiting the program.");
+        break;
+    }
+    Console.WriteLine($"You entered: {input}");
+
+} while (input != "exit");
