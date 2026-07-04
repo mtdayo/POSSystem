9f28eb212414aa408fa2d80fee3ec65be00ea276
mtdayo <lawrightmt@gmail.com>
2026-07-04 17:30:17 +0900

    Update Program.cs

--- POSSystem.Console/Program.cs
+++ POSSystem.Console/Program.cs
@@ -11,24 +11,34 @@ foreach (var product in service.GetAllProducts())
     Console.WriteLine($"コード: {product.Code}, 名前: {product.Name}, 価格: {product.Price}, 在庫: {product.Stock}");
 }
 
-Console.WriteLine();
 
-Console.Write("商品コードを入力してください: ");
-string? code = Console.ReadLine();
 
-var productInfo = service.GetProductByCode(code!);
-
-if (productInfo == null)
-{
-    Console.WriteLine("商品が見つかりませんでした。");
-}
-else
+while (true)
 {
     Console.WriteLine();
-    Console.WriteLine("商品情報");
-    Console.WriteLine($"商品名: {productInfo.Name}");
-    Console.WriteLine($"価格: {productInfo.Price}");
-    Console.WriteLine($"在庫: {productInfo.Stock}");
+    Console.Write("商品コードを入力してください: ");
+    string? code = Console.ReadLine();
+
+    var productInfo = service.GetProductByCode(code!);
+
+    if (code!.Equals("exit", StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+
+
+    if (productInfo == null)
+    {
+        Console.WriteLine("商品が見つかりませんでした。");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("商品情報");
+        Console.WriteLine($"商品名: {productInfo.Name}");
+        Console.WriteLine($"価格: {productInfo.Price}");
+        Console.WriteLine($"在庫: {productInfo.Stock}");
+    }
 }
 
 
